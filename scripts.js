81de74005fbb3ed668b644611016cfc2345d0d89
1Brenny1 <101068740+1Brenny1@users.noreply.github.com>
2022-10-03 09:41:18 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function Btn_Home() {
+    
+}
+function Btn_Games() {
+    document.location.href = "games/index.html"
+}
+function Btn_Info() {
+    document.location.href = "info/index.html"
+}
+function Loaded() {
+    console.log("Loaded")
+    document.getElementById("loader").style.display = "none"
+}
