03b8e1088d9117fc4b0fa22d69933dbe89c0661d
1Brenny1 <101068740+1Brenny1@users.noreply.github.com>
2022-10-03 21:52:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // https://coolors.co/b9d6f2-061a40-0353a4-006daa-003559
 if (document.location.href.toLowerCase() == "http://www.brenny.tk/") {
+    console.log("Unsecure connection, switching to secure connection")
     document.location.href = "https://www.brenny.tk/"
 }
