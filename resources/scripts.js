1b6814282e7502256ec7883933c6872da2c3c03f
1Brenny1 <101068740+1Brenny1@users.noreply.github.com>
2023-01-13 11:57:14 -0500

    Add files via upload

---
+++ resources/scripts.js
@@ -0,0 +1,36 @@
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      file = elmnt.getAttribute("w3-include-html");
+      if (file) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        return;
+      }
+    }
+  }
+
+includeHTML()
+/*
+if (location.href.includes("projects/")) {
+  var v = document.getElementsByTagName("a");
+  for (i = 0; i < v.length; i++) {
+    var elmnt = v[i];
+    var Dir = elmnt.getAttribute("href")
+    if (Dir) {
+      elmnt.setAttribute("href", "../" + Dir)
+    }
+  }
+}
+*/
